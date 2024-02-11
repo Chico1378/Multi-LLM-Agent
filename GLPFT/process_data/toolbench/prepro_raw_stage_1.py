883bf5602ee85c4fab3067a134b2b6f0b337a05b
Chico <113044814+Chico1378@users.noreply.github.com>
2024-02-11 19:20:12 +0800

    Update prepro_raw_stage_1.py

--- GLPFT/process_data/toolbench/prepro_raw_stage_1.py
+++ GLPFT/process_data/toolbench/prepro_raw_stage_1.py
@@ -8,8 +8,8 @@ import tqdm
 print("####################### PREPRO RAW DATA STAGE 1 #####################")
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', type=str, default="")
-parser.add_argument('--output_path', type=str, default='')
+parser.add_argument('--data_dir', type=str, default="") #data_dir 获得了$ORI_DATA_DIR的路径 "../data/toolbench/data"
+parser.add_argument('--output_path', type=str, default='') #output_path 获得了RAW_DATA_OUT_DIR的路径 "dataset/toolbench/train/raw_data"
 
 args = parser.parse_args()
 
@@ -22,27 +22,27 @@ print('test_ids', test_ids)
 
 data = []
 for train_set in ["G1_answer", "G2_answer", "G3_answer"]:
-    for file in tqdm.tqdm(os.listdir(f"{args.data_dir}/answer/{train_set}")):
+    for file in tqdm.tqdm(os.listdir(f"{args.data_dir}/answer/{train_set}")): #tqdm加了进度条
         id = file.split('_')[0]
         if id not in test_ids:
             with open(f"{args.data_dir}/answer/{train_set}/{file}") as f:
-                d = json.load(f)
-            instruction = d['answer_generation']['query']
+                d = json.load(f)   #d是文件夹G1/G2/G3 answer中一个json文件的内容
+            instruction = d['answer_generation']['query'] #一个json文件一个query
             new_tools = []
-            for t in d['answer_generation']['function']:
+            for t in d['answer_generation']['function']: #t是'function'下的子函数
                 # tool_name = change_name(standardize(t['api_name'])) + '_for_' + standardize(t['tool_name'])
                 tool_name = t['name']
-                if tool_name != "Finish":
+                if tool_name != "Finish":   #当name=finish的时候，才是结束这个函数的调用
                     tool_name = tool_name[-64:]
                     tool_function = t['description'][:256]
                     tool_input = {}
-                    for arg_name,arg_value in t['parameters']['properties'].items():
-                        arg_type = arg_value['type']
-                        if 'description' in arg_value.keys():
+                    for arg_name,arg_value in t['parameters']['properties'].items(): #子函数-parameters-properties-（参数，值）
+                        arg_type = arg_value['type']      
+                        if 'description' in arg_value.keys():  #检查当前遍历的参数值中是否包含description键
                             tool_input[arg_name] = arg_type + ', ' + arg_value['description'][-256:]
                         else:
                             tool_input[arg_name] = arg_type
-                    # print(json.dumps(t, indent=2))
+                    # print(json.dumps(t, indent=2)) #将t转化为json格式
                     new_tools.append({
                         'Name': tool_name,
                         'function':tool_function,
@@ -51,9 +51,10 @@ for train_set in ["G1_answer", "G2_answer", "G3_answer"]:
             data.append({
                 'tools':new_tools,
                 'instruction':instruction,
-                'chains':d['answer_generation']['train_messages'][-1]
+                'chains':d['answer_generation']['train_messages'][-1] #-1是取最后一次训练的回答。每次train_message都循序渐进，没调用-开始调用-问询-函数查询结果
             })
-
+# instruciton 是answer_generation中的query，chain是训练得到的最终回答
+# data中是  工具-问询-训练后获得的回答 的列表
     print(train_set,'data processed')
             
 os.makedirs(args.output_path, exist_ok=True)
